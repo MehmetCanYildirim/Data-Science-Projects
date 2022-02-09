3554297426a6a52fadb0750faf7aa7a633b1513f
MehmetCanYildirim <80276063+MehmetCanYildirim@users.noreply.github.com>
2022-02-09 16:13:13 +0300

    Create Feature Engineering on Telco-customer-churn dataset.py

---
+++ Feature Engineering/Feature Engineering on Telco-customer-churn dataset.py
@@ -0,0 +1,314 @@
+#Gerekli Kütüphanelerin Yüklenmesi
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from catboost import CatBoostClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,roc_auc_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, cross_validate
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+import warnings
+warnings.simplefilter(action="ignore")
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+
+def load_telco():
+    df = pd.read_csv(r"Ödevler/Ödevler/Telco-Customer-Churn.csv")
+    return df
+
+df = load_telco()
+
+df.head()
+
+df.shape
+df.info()
+df.columns
+df.describe().T
+df.index
+df.isnull().sum()
+
+# Genel Resim
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
+
+# Verisetinde Numerik ama object olarak gözüken değişkenin içerisine girip bir bakmak gerekti.
+df[df["TotalCharges"] == " "]
+
+df["TotalCharges"] = df["TotalCharges"].replace([" "], np.NaN)
+
+df["TotalCharges"] = df["TotalCharges"].astype("float")
+
+df.info()
+
+# Kategorik ve Sayısal Değişkenleri Gözlemleyelim.
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optional
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+
+    """
+
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+
+    cat_cols = cat_cols + num_but_cat
+
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+# Kategorik değişkenlerin hedef değişken ile analizi
+
+df.loc[df["Churn"] == "No", "Churn"] = 0
+df.loc[df["Churn"] == "Yes", "Churn"] = 1
+df["Churn"] = df["Churn"].astype("int")
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+
+    print(pd.DataFrame({"Target_mean": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+
+for col in cat_cols:
+    target_summary_with_cat(df, "Churn", col)
+
+# Numerik değişkenlerin hedef değişken ile analizi
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+for col in num_cols:
+    target_summary_with_num(df, "Churn", col)
+
+
+# Korelasyon analizi.
+
+corr = df[num_cols].corr()
+
+sns.set(rc={'figure.figsize': (12, 12)})
+sns.heatmap(corr, cmap="RdBu")
+plt.show()
+
+cor_matrix = df.corr().abs()
+
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+high_correlated_cols(df, plot=True)
+
+
+# Tenure'e bakıldığında 1 aylık müşterilerin çok fazla olduğunu ardından da 70 aylık müşterilerin geldiğini görüyoruz.
+# Farklı kontratlardan dolayı gerçekleşmiş olabilir, aylık sözleşmesi olan kişilerin tenure ile 2 yıllık sözleşmesi olan kişilerin tenure'ne bakalım.
+df[df["Contract"] == "Month-to-month"]["tenure"].hist(bins=20)
+plt.xlabel("tenure")
+plt.title("Month-to-month")
+plt.show()
+
+df[df["Contract"] == "Two year"]["tenure"].hist(bins=20)
+plt.xlabel("tenure")
+plt.title("Two year")
+plt.show()
+
+# MonthyChargers'a bakıldığında aylık sözleşmesi olan müşterilerin aylık ortalama ödemeleri daha fazla olabilir.
+df[df["Contract"] == "Month-to-month"]["MonthlyCharges"].hist(bins=20)
+plt.xlabel("MonthlyCharges")
+plt.title("Month-to-month")
+plt.show()
+
+df[df["Contract"] == "Two year"]["MonthlyCharges"].hist(bins=20)
+plt.xlabel("MonthlyCharges")
+plt.title("Two year")
+plt.show()
+
+
+
+# Missing Values
+df.isnull().sum()
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+    if na_name:
+        return na_columns
+
+na_columns = missing_values_table(df, na_name=True)
+
+df["TotalCharges"].fillna(df["TotalCharges"].median(), inplace=True)
+
+df.isnull().sum()
+
+# Outliers
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable, q1=0.05, q3=0.95)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+# Aykırı Değer Analizi ve Baskılama İşlemi
+for col in num_cols:
+    print(col, check_outlier(df, col))
+    if check_outlier(df, col):
+        replace_with_thresholds(df, col)
+
+
+df.head()
+
+# Feature Extraction
+# Tenure  değişkeninden yıllık kategorik değişken oluşturma
+df.loc[(df["tenure"]>=0) & (df["tenure"]<=12),"NEW_TENURE_YEAR"] = "0-1 Year"
+df.loc[(df["tenure"]>12) & (df["tenure"]<=24),"NEW_TENURE_YEAR"] = "1-2 Year"
+df.loc[(df["tenure"]>24) & (df["tenure"]<=36),"NEW_TENURE_YEAR"] = "2-3 Year"
+df.loc[(df["tenure"]>36) & (df["tenure"]<=48),"NEW_TENURE_YEAR"] = "3-4 Year"
+df.loc[(df["tenure"]>48) & (df["tenure"]<=60),"NEW_TENURE_YEAR"] = "4-5 Year"
+df.loc[(df["tenure"]>60) & (df["tenure"]<=72),"NEW_TENURE_YEAR"] = "5-6 Year"
+
+
+# Kontratı 1 veya 2 yıllık müşterileri Engaged olarak belirtme
+df["NEW_Engaged"] = df["Contract"].apply(lambda x: 1 if x in ["One year","Two year"] else 0)
+
+# Herhangi bir destek, yedek veya koruma almayan kişiler
+df["NEW_noProt"] = df.apply(lambda x: 1 if (x["OnlineBackup"] != "No") or (x["DeviceProtection"] != "No") or (x["TechSupport"] != "No") else 0, axis=1)
+
+# Aylık sözleşmesi bulunan ve genç olan müşteriler
+df["NEW_Young_Not_Engaged"] = df.apply(lambda x: 1 if (x["NEW_Engaged"] == 0) and (x["SeniorCitizen"] == 0) else 0, axis=1)
+
+
+# Kişinin toplam aldığı servis sayısı
+df['NEW_TotalServices'] = (df[['PhoneService', 'InternetService', 'OnlineSecurity',
+                                       'OnlineBackup', 'DeviceProtection', 'TechSupport',
+                                       'StreamingTV', 'StreamingMovies']]== 'Yes').sum(axis=1)
+
+
+# Herhangi bir streaming hizmeti alan kişiler
+df["NEW_FLAG_ANY_STREAMING"] = df.apply(lambda x: 1 if (x["StreamingTV"] == "Yes") or (x["StreamingMovies"] == "Yes") else 0, axis=1)
+
+# Kişi otomatik ödeme yapıyor mu?
+df["NEW_FLAG_AutoPayment"] = df["PaymentMethod"].apply(lambda x: 1 if x in ["Bank transfer (automatic)","Credit card (automatic)"] else 0)
+
+# ortalama aylık ödeme
+df["NEW_AVG_Charges"] = df["TotalCharges"] / (df["tenure"] +0.1)
+
+# Güncel Fiyatın ortalama fiyata göre artışı
+df["NEW_Increase"] = df["NEW_AVG_Charges"] / (df["MonthlyCharges"] + 1)
+
+# Servis başına ücret
+df["NEW_AVG_Service_Fee"] = df["MonthlyCharges"] / (df['NEW_TotalServices'] + 1)
+
+# Encoding
+# Değişkenlerin tiplerine göre ayrılması işlemi
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+cat_cols = [col for col in cat_cols if col not in ['Churn','NEW_TotalServices']]
+cat_cols
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+
